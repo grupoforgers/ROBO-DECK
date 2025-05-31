5a220acdf5337884b27f436ff28c971b35838f2d
grupoforgers <forgersgroup@gmail.com>
2025-05-31 15:23:26 -0300

    8

--- src/web_interface_node/build/web_interface_node/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/forgers/ros2_ws/src/web_interface_node
-# Build directory: /home/forgers/ros2_ws/src/web_interface_node/build/web_interface_node
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
